1ffa5362d8feec910f09659daf1ca1ca7d45bb01
jorgezapatal-bit <jorge.zapatal@unmsm.edu.pe>
2026-04-24 10:46:59 -0500

    Hola mundo

---
+++ src/main/java/com/mycompany/session01/Alumno.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.session01;
+
+/**
+ *
+ * @author F20LAB207E21
+ */
+public class Alumno {
+    
+}
